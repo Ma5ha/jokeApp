f56795f81fb71a1c841e9a1d184eebb5b4381c27
Ma5ha <mladen.blagovcanin@klika.ba>
2020-12-03 11:55:01 +0100

    added base api to enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiBase: 'https://sv443.net/jokeapi/v2',
+  
 };
 
 /*
